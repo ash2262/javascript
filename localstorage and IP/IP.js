032c074b69627efbf3de0cbdf11df67da5e02720
Ashish Sahu <ash2262@gmail.com>
2020-09-15 16:46:24 +0530

    changes done to check if the value is already there in localstorage or not

--- localstorage and IP/IP.js
+++ localstorage and IP/IP.js
@@ -1,57 +1,21 @@
-var ip = localStorage.getItem("IP");
+var ip = null;
 console.log("localstorageIP: "+ip);
 async function fetchIp(){
 	try {
-		
-		ip = await fetch('https://mocktarget.apigee.net/ip')
-		.then(res => res.json())
-		.then(res => res.ip)
-		.then(res => res);
-		console.log("ip:"+ip);
+		ip = localStorage.getItem("IP");    //fetch IP from localstorage
+		if(ip==null){ 						// if localstorage value is null, fetch from API
+			console.log("TEst iip:"+ip);
+			ip = await fetch('https://mocktarget.apigee.net/ip')
+			.then(res => res.json())
+			.then(res => res.ip)
+			.then(res => res.split(",")[0]);
+			
+			localStorage.setItem("IP",ip); // set IP in localstorage
+		}
+		console.log("ip-address:"+ip);
+	return ip;
 	}
 	catch(error) {
 		console.log(error);
 	}
 };
-
-(async function () {
-
-if(ip==null)
-{
-	await fetchIp();
-}
-	
-	if (document.addEventListener) {
-	   //console.log("ip_testing1:"+ip);
-	  document.addEventListener("onBoomerangLoaded", function(e){
-		console.log("ip_testing2:"+ip);
-		BOOMR.init({
-		  beacon_url: "https://boomerangprod.worldbank.org",
-		  beacon_type: "GET",
-		  beacon_url_force_https: false,
-		  strip_query_string: false,
-		  log: null,
-		  ResourceTiming: {
-			enabled: false,
-			clearOnBeacon: true
-		  },
-		  RT: {
-			enabled: false,
-			cookie: "WBG-Cookie-Sample"
-		  },
-		  NavigationTiming: {
-			enabled: true
-		  }
-		});
-		console.log("ip_testing:"+ip);
-		BOOMR.addVar({
-		  "Tag_ip":ip
-		});
-		BOOMR.t_end = new Date().getTime();
-	  });
-	}
-	console.log("ip1:"+ip);
-	//localStorage.setItem("IP",ip);
-	console.log("localstorageValue:"+localStorage.getItem("IP"));
-})();
-
